85040d7d811dc8fb7ab4225a2cc2ebdb3793e116
yutakakomada <y.komada.01@gmail.com>
2017-11-07 13:26:20 +0000

    add signup of new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザー登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
