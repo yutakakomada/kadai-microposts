90f393e3f7493927b501af7c522a76af1999d230
yutakakomada <y.komada.01@gmail.com>
2017-11-12 06:15:44 +0000

    add microposts#create destroy

--- app/controllers/toppages_controller.1.rb
+++
@@ -1,9 +0,0 @@
-class ToppagesController < ApplicationController
-  def index
-    if logged_in?
-      @user = current_user
-      @micropost = current_user.microposts.build  # form_for 用
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
-    end
-  end
-end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -7,3 +7,4 @@ class ToppagesController < ApplicationController
     end
   end
 end
+
