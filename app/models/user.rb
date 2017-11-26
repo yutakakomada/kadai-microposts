ca3ce975f2507f83f05abcff4417bdc7188ac5eb
yutakakomada <y.komada.01@gmail.com>
2017-11-26 05:06:03 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,6 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user 
-
-#  def follow(other_user)
-#    unless self == other_user
-#      self.relationships.find_or_create_by(follow_id: other_user.id)
-#    end
-#  end
   
   def follow(other_user)
     unless self == other_user
@@ -31,5 +25,9 @@ class User < ApplicationRecord
   
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
   end
 end
